93353dbd3d83de5c63777eec47fe0f0518695e15
Lucca Radaeli <radaeli@MacBook-Pro-de-Lucca.local>
2020-01-20 16:49:25 -0300

    Add PROCFILE HEROKU

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const server = http.Server(app)
 
 setupWebSocket(server)
 
-mongoose.connect('mongodb://127.0.0.1:27017/devRadar',
+mongoose.connect(process.env.MONGO_URL,
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
