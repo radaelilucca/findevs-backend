1aff7aef25317e691ce615a20407b17391c9a1e2
radaelilucca <radaelidev@gmail.com>
2020-01-26 20:56:32 -0800

    auth middleware setup

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import DevController from './controllers/DevController'
 import SearchController from './controllers/SearchController'
 import SessionController from './controllers/SessionController'
 
+import authMiddleware from './middlewares/auth'
+
 const routes = Router();
 
 // create a dev
@@ -11,6 +13,10 @@ routes.post('/devs', DevController.store)
 
 // login
 routes.post('/login', SessionController.store)
+
+// Auth required routes
+
+routes.use(authMiddleware)
 
 
 // list devs
