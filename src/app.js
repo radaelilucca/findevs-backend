9fcb4bf1a0db88c3b4a604d7b9e576d5ec6855fe
radaelilucca <radaelidev@gmail.com>
2020-01-29 20:08:36 -0800

    toUpper techs for new users

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-import express from 'express'
-import http from 'http'
-import routes from './routes'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import {setupWebSocket} from './websocket'
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import routes from './routes';
+import { setupWebSocket } from './websocket';
 
 const app = express();
-const server = http.Server(app)
+const server = http.Server(app);
 
-setupWebSocket(server)
+setupWebSocket(server);
 
-mongoose.connect(process.env.MONGO_URL,
-{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(
+  process.env.MONGO_URL ||
+    'mongodb+srv://devmode:devmode@cluster0-sqkla.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-
-
-export default server
+export default server;
